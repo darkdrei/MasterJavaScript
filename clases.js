fd5c5a6f5eba9257382df4d134c5280b0ec3f202
pajaro <eugenio.pajaro.ext@nokia.com>
2019-09-17 23:44:59 -0500

    Uso de herrencia en JavaScript

--- clases.js
+++ clases.js
@@ -14,7 +14,23 @@ class Coche{
     }
 }
 
-var coche1 = new Coche('BMW', 200, 2017);
+
+/**
+ * Uso de Herrencia
+ */
+
+ class Autobus extends Coche{
+    constructor(modelo, velocidad, antiguedad, altura){
+         super(modelo, velocidad, antiguedad);
+         this.altura = altura;
+    }
+
+    mostrarAltura(){
+        console.log('Mostrar altura ',this.altura);
+    }
+ }
+
+var coche1 = new Autobus('BMW', 200, 2017);
 var coche2 = new Coche('AUDI', 100, 2018);
 var coche3 = new Coche('MERCEDES', 150, 2011);
 var coche4 = new Coche('Renault', 180, 2016);
